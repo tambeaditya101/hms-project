e9b715742409e6413ac6e3b697643ba5c5e35648
adityatambe-personal <tambeaditya101@gmail.com>
2025-12-12 18:41:33 +0530

    frontend env add

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../store/index.js";
 
 const api = axios.create({
-  baseURL: "https://hms-project-be.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
